97b68d54da6619515ecc4d506a0ff57af4c5bb43
parbrigal <30548384+parbrigalDev@users.noreply.github.com>
2019-08-09 06:33:29 +0200

    Merge

--- client/src/store/reducers/user_reducer.js
+++ client/src/store/reducers/user_reducer.js
@@ -1,17 +1,19 @@
-import { LOGIN_USER,REGISTER_USER, AUTH_USER, LOGOUT_USER,ADD_CART_TO_USER} from '../actions/types';
+import { LOGIN_USER,REGISTER_USER, AUTH_USER, LOGOUT_USER,ADD_CART_TO_USER,GET_CART_ITEMS_USER} from '../actions/types';
 
 export default function(state={},action) {
     switch(action.type) {
         case LOGIN_USER:
-        return { ...state,loginSuccess:action.payload }
+            return { ...state,loginSuccess:action.payload }
         case REGISTER_USER:
-        return { ...state,registerSuccess:action.payload }
+            return { ...state,registerSuccess:action.payload }
         case AUTH_USER:
-        return { ...state,userData: action.payload }
+            return { ...state,userData: action.payload }
         case LOGOUT_USER:
-        return { ...state}
+            return { ...state}
+        case GET_CART_ITEMS_USER: 
+            return {...state, cartDetail: action.payload}
         case ADD_CART_TO_USER:
-        return { ...state ,userData : {
+            return { ...state ,userData : {
             ...state.userData,
             cart:action.payload
         }}
